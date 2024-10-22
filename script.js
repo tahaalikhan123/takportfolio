a36dee5e52a8f756457c868444cf2a79374fac9e
tahaalikhan123 <tahaalikhan30@gmail.com>
2024-10-22 12:09:30 +0400

    new styles

--- script.js
+++ script.js
@@ -182,6 +182,47 @@ document.addEventListener("DOMContentLoaded", function () {
       // You can add more functionality here, like opening a modal or navigating to a signup page
     });
   }
+
+  // Typing effect
+  const typedTextSpan = document.querySelector("#typed-text");
+  const cursorSpan = document.querySelector(".cursor");
+
+  const textArray = ["Data Analyst", "Data Engineer", "Problem Solver"];
+  const typingDelay = 200;
+  const erasingDelay = 100;
+  const newTextDelay = 2000;
+  let textArrayIndex = 0;
+  let charIndex = 0;
+
+  function type() {
+    if (charIndex < textArray[textArrayIndex].length) {
+      if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+      typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+      charIndex++;
+      setTimeout(type, typingDelay);
+    } 
+    else {
+      cursorSpan.classList.remove("typing");
+      setTimeout(erase, newTextDelay);
+    }
+  }
+
+  function erase() {
+    if (charIndex > 0) {
+      if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+      typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+      charIndex--;
+      setTimeout(erase, erasingDelay);
+    } 
+    else {
+      cursorSpan.classList.remove("typing");
+      textArrayIndex++;
+      if(textArrayIndex>=textArray.length) textArrayIndex=0;
+      setTimeout(type, typingDelay + 1100);
+    }
+  }
+
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
 });
 
 // Function to handle form submission response
